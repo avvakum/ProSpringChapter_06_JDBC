eebf772e9de7449d765869ac05c6309e016ba675
AVVA <abba501@mail.ru>
2026-05-28 12:20:28 +0300

    28.05.2026

---
+++ src/main/java/com/avva/six/config/JndiDataSourceCfg.java
@@ -0,0 +1,39 @@
+package com.avva.six.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jndi.JndiTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * Assume Datasource is configured as a resource in an application such as Apache Tomcat
+ * {@code
+<Resource name="jdbc/musicdb"
+auth="Container"
+type="javax.sql.DataSource"
+driverClassName="org.mariadb.jdbc.Driver"
+url="jdbc:mariadb://localhost:3306/musicdb"
+username="root"
+password="root"
+maxTotal="20"
+maxIdle="10"
+maxWaitMillis="-1"/>
+}
+ */
+@Configuration
+public class JndiDataSourceCfg {
+    private static Logger LOGGER = LoggerFactory.getLogger(JndiDataSourceCfg.class);
+
+    @Bean
+    public DataSource dataSource() {
+        try {
+            return (DataSource) new JndiTemplate().lookup("java:comp/env/jdbc/musicdb");
+        } catch (Exception e) {
+            LOGGER.error("JNDI DataSource bean cannot be created!", e);
+            return null;
+        }
+    }
+}
